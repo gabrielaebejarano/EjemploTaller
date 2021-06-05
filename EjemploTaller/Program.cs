a49d8dcfe84f7b89e20a7621c88d538feba5e276
gbejarano <gabriela_bejarano@hotmail.com>
2021-06-05 10:13:20 -0300

    Agregar archivos de proyecto.

---
+++ EjemploTaller/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EjemploTaller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
