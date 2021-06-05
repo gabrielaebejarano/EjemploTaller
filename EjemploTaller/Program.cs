de8261d5225e7b5d9a27005e9377ea1b1eb05921
gbejarano <gabriela_bejarano@hotmail.com>
2021-06-05 10:23:24 -0300

    Se agrego comentario

--- EjemploTaller/Program.cs
+++ EjemploTaller/Program.cs
@@ -7,6 +7,7 @@ namespace EjemploTaller
         static void Main(string[] args)
         {
             Console.WriteLine("Hola mundo");
+            Console.WriteLine("Comnetario 1");
         }
     }
 }
