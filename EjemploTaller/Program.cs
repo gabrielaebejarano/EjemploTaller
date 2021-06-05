afbe8df86409c7c6b31b45b826085f9311af087d
gbejarano <gabriela_bejarano@hotmail.com>
2021-06-05 10:14:24 -0300

    Mensaje de bienvenida

--- EjemploTaller/Program.cs
+++ EjemploTaller/Program.cs
@@ -6,7 +6,7 @@ namespace EjemploTaller
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hola mundo");
         }
     }
 }
